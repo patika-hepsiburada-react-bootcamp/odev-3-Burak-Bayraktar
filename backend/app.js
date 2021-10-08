950b1b68903749c62da047694fa4f8d3e537f58d
Burak-Bayraktar <burak.bayraktar@akinon.com>
2021-10-08 18:49:18 +0300

    Change port number

--- backend/app.js
+++ backend/app.js
@@ -31,9 +31,9 @@ io.on('connection', (socket) => {
 
 
 // listen port
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-    console.log("listening on *:3002")
+    console.log("listening on *:5000")
 })
 
 
